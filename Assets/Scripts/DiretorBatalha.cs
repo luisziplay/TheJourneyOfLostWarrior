50c8341763a8e7526b1522b9837cfc341a00a612
Luis Gustavo <pankadinhagdf@gmail.com>
2025-04-24 17:57:35 -0300

    Terminei o codigo do Player
    Falta terminar o codigo DiretorBatalha e ajeitar os problemas

--- Assets/Scripts/DiretorBatalha.cs
+++ Assets/Scripts/DiretorBatalha.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DiretorBatalha : MonoBehaviour
@@ -8,12 +10,12 @@ public class DiretorBatalha : MonoBehaviour
     [SerializeField] Player inimigo;
     [SerializeField] TextMeshProUGUI vidaPlayer;
     [SerializeField] TextMeshProUGUI vidaInimigo;
-    [SerializeField] TextMeshProUGUI nomePlayer;
-    [SerializeField] TextMeshProUGUI nomeInimigo;
+    /*[SerializeField] TextMeshProUGUI nomePlayer;*/
+    /*[SerializeField] TextMeshProUGUI nomeInimigo;*/
     [SerializeField] TextMeshProUGUI informativo;
-    [SerializeField] TextMeshProUGUI indicadorEspecial;
-    [SerializeField] GameObject textoTextoVitoria;
-    [SerializeField] GameObject textoTextoDerrota;
+    /*[SerializeField] TextMeshProUGUI indicadorEspecial;*/
+    /*[SerializeField] GameObject textoTextoVitoria;*/
+    /*[SerializeField] GameObject textoTextoDerrota;*/
     [SerializeField] Button botaoEspecial;
     [SerializeField] Button botaoAtaque;
     string turno = "Player";
@@ -24,12 +26,123 @@ public class DiretorBatalha : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        /*vidaPlayer.text = player.GetVida().ToString();
+        vidaInimigo.text = inimigo.GetVida().ToString();
+        indicadorEspecial.text = player.ValorEspecial().ToString();*/
+        botaoEspecial.interactable = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        AtualizaDadosTela();
 
+        if (turno == "Player" && verificadorDeTurno && player.VerificaVida())
+        {
+            botaoAtaque.interactable = true;
+
+            if (player.VerificaEspecial())
+            {
+                botaoEspecial.interactable = true;
+            }
+            else
+            {
+                botaoEspecial.interactable = false;
+            }
+
+            verificadorDeTurno = false;
+        }
+        else if (turno == "Inimigo" && verificadorDeTurno && inimigo.VerificaVida())
+        {
+            StartCoroutine(AtaqueInimigo());
+        }
+
+        VerificaVitoria();
+
+    }
+    private void AtualizaDadosTela()
+    {
+        vidaPlayer.text = player.GetVida().ToString();
+        vidaInimigo.text = inimigo.GetVida().ToString();
+    }
+    public void AtaquePlayer()
+    {
+        inimigo.LevarDano(player.Atacar());
+        StartCoroutine(AtaqueP());
+    }
+
+    public void AtaqueEspecial()
+    {
+        inimigo.LevarDano(player.Especial());
+        StartCoroutine(AtaqueP()); 
+    }
+
+    
+
+    private IEnumerator AtaqueInimigo()
+    {
+        verificadorDeTurno = false;
+
+        if (turno == "Inimigo")
+        {
+            botaoAtaque.interactable = false;
+            botaoEspecial.interactable = false;
+            player.LevarDano(inimigo.Atacar());
+            yield return new WaitForSeconds(5f);
+            verificadorDeTurno = true;
+            turno = "Player";
+        }
+    }
+
+    private IEnumerator AtaqueP()
+     {
+        verificadorDeTurno = false;
+        botaoAtaque.interactable = false;
+        botaoEspecial.interactable = false;
+        /*indicadorEspecial.text = player.ValorEspecial().ToString();*/
+
+        if (turno == "Player")
+        {
+            yield return new WaitForSeconds(5f);
+            verificadorDeTurno = true;
+            turno = "Inimigo";
+        }
+    }
+    public void RecebeTexto(string texto)
+    {
+        StartCoroutine(ExibeTexto(texto));
+    }
+
+    private IEnumerator ExibeTexto(string texto)
+    {
+        informativo.text += texto + "\n";
+        yield return new WaitForSeconds(5f);
+        informativo.text = "";
+    }
+
+    IEnumerator TelaVitoria()
+    {
+        yield return new WaitForSeconds(2.0f);
+        /*player.PlaySomVitoria();*/
+        yield return new WaitForSeconds(1.0f);
+        /*textoTextoVitoria.SetActive(true);*/
+    }
+
+    public void VerificaVitoria()
+    {
+        if (!inimigo.VerificaVida())
+        {
+            StartCoroutine(TelaVitoria());
+        }
+        else if (!player.VerificaVida())
+        {
+           /* player.PlaySomMorte();*/
+           /*textoTextoDerrota.SetActive(true);*/
+        }
+    }
+    public void ReiniciarJogo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,57 +1,141 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class Player : MonoBehaviour
-{
-    public class Players : MonoBehaviour
+
+
+    public class Player : MonoBehaviour
     {
         [SerializeField] private int ataque;
         [SerializeField] private int defesa;
         [SerializeField] private int vida;
         [SerializeField] private int especial;
+        [SerializeField] private int ataqueEspecial;
+        [SerializeField] private bool estahVivo;
         [SerializeField] private DiretorBatalha dB;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public int GetVida()
+        {
+            return vida;
+        }
+
+        public bool VerificaVida() //retorna true se o jogador esta vivo e false se esta morto
+        {
+            return estahVivo;
+        }
+
+        public bool VerificaEspecial() //retorna true se o jogador tem especial e false se nao tem
+        {
+            if (especial >= 3)
+            {
+                dB.RecebeTexto($"O seu especial esta pronto!");
+                /* audioSource.PlayOneShot(somEspecialPronto);*/
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int ValorEspecial()
+        {
+            return especial;
+        }
+
+
         public int Atacar()
         {
             int valorAtaque = Random.Range(10, 30);
             especial++;
+
+            if (valorAtaque > 0)
+            {
+                dB.RecebeTexto("ARgh! Sinta Minha Furia!");
+                dB.RecebeTexto($"Voce ataca com {valorAtaque}");
+                /*PlaySomAtaque();*/
+            }
+            else
+            {
+                dB.RecebeTexto($"Voce erra o ataque.");
+                /*PlaySomErroAtaque();*/
+            }
             return ataque;
+
         }
         public int Defesa()
         {
-            int valordefesa = Random.Range(10, 30);
+            int valorDefesa = Random.Range(10, 30);
+
+            if (valorDefesa > 0)
+            {
+                dB.RecebeTexto($"Voce defende com {valorDefesa}");
+            }
+            else
+            {
+                dB.RecebeTexto("Voce nao consegue defender.");
+            }
             return defesa;
         }
-        public int LevarDano(int dano)
+    public void LevarDano(int dano)
+    {
+        int danoFinal = dano - Defesa();
+
+        if (estahVivo)
         {
-            int danoFinal = dano - Defesa();
-            return danoFinal;
+            Debug.Log("");
+            Debug.Log($"sua vida e: {vida}");
         }
+        else
+        {
+            dB.RecebeTexto($"Voce , morreu!");
+        }
+    }
         public int Especial()
         {
             if (especial == 3)
             {
-                int ataqueEspecial = ataque * 2;
-                return ataqueEspecial;
+                int valorEspecial = Random.Range(20, ataque);
+                int chanceDeDobrar = Random.Range(0, 100);
+                int fatorMultiplicador = especial;
+
+                if (chanceDeDobrar >= 90 && especial >= 3)
+                {
+                    int valorEspecialDobrado = (valorEspecial * 2) + fatorMultiplicador;
+                    dB.RecebeTexto("ARgh! Sede de Vinguança!");
+                    dB.RecebeTexto($"Voce ataca com {valorEspecialDobrado}");
+                    /*PlaySomEspecial();*/
+                    especial = 0;
+                    return valorEspecialDobrado;
+                }
+                else if (chanceDeDobrar < 90 && especial >= 3)
+                {
+                    dB.RecebeTexto("ARgh! Vou te esmagar!");
+                    dB.RecebeTexto($"Voce ataca com {valorEspecial}");
+                    /*PlaySomAtaque();*/
+                    especial = 0;
+                    return valorEspecial;
+                }
+                else
+                {
+                    dB.RecebeTexto("Seu especial nao esta carregado!");
+                    return 0;
+                }
             }
             else
             {
                 dB.RecebeTexto("O seu especial nao esta carregado");
+                return 0;
             }
         }
 
 
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
         {
 
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
-}
+
